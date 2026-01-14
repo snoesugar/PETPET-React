a9fdefe884497295eba4657b11ef87d085fe8b2f
happy <daisy831015@gmail.com>
2026-01-14 11:13:58 +0800

    字體、navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,27 @@ function App() {
 
   return (
     <>
-
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">PETPET</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">預約服務</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">成為寵物保姆</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">登入</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }
