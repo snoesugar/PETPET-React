de3586a29a0e740a5f1ab39e408c7e8c35a89255
happy <daisy831015@gmail.com>
2026-01-14 10:53:40 +0800

    修改部屬路徑

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/PETPET-React/' : '/',
+  base: '/PETPET-React/',
   plugins: [react()],
 })
